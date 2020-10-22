f0b54aab57b5929d6ff38174c878dabfca2bc385
ezgihendrickx <ezgihendrickx@gmail.com>
2020-10-22 17:42:51 +0200

    Add the keys

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,16 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./app.css";
 export default function TodoList() {
-  const todos = ["Learn react", "Wash your hands", "Wash the dishes"];
+  const initialTodos = ["Learn react", "Wash your hands", "Wash the dishes"];
+  const [todos, setTodos] = useState(initialTodos); //const myState = useState (...);
+  //initialTodos is default when you open the browser it will be saved in the todos
+  //and Settodos is a function then to update your list
   return (
-    <div>
+    <div className="Todo">
       <h3>Todos</h3>
       <ul>
-        {todos.map((todo) => (
-          <li>
+        {todos.map((todo, i) => (
+          <li key={"todo_" + i}>
             <input type="checkbox"></input> {todo}
           </li>
         ))}
